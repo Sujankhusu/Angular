537ce70bddf9f9e2965a625d8643036b28c882d0
Sujankhusu <khususujan00@gmail.com>
2022-01-08 15:05:17 +0545

    create reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+
+  registrationForm = new FormGroup({
+    userName: new FormControl('Sujan'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city:new FormControl(''),
+      state:new FormControl(''),
+      postalCode:new FormControl('')
+    })
+  });
 }
